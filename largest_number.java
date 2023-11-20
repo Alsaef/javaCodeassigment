61e31a33e0cf84652f79403bc56a6f1c4b933c84
Alsaef <110528853+Alsaef@users.noreply.github.com>
2023-11-20 14:19:20 +0600

    Create largest_number.java

---
+++ largest_number.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the first number:");
+        int num1 = scanner.nextInt();
+
+        System.out.println("Enter the second number:");
+        int num2 = scanner.nextInt();
+
+        System.out.println("Enter the third number:");
+        int num3 = scanner.nextInt();
+
+        int largest = findLargest(num1, num2, num3);
+
+        System.out.println("The largest number is: " + largest);
+    }
+
+    // Function to find the largest of three numbers
+    private static int findLargest(int a, int b, int c) {
+        if (a >= b && a >= c) {
+            return a;
+        } else if (b >= a && b >= c) {
+            return b;
+        } else {
+            return c;
+        }
+    }
+}
